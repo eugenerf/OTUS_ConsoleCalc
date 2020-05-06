74400f1ca2df58e718dfd9bf83d9f6966490def2
eugene <eugenerf@mail.ru>
2020-05-06 16:22:17 +0300

    Added the Number expression

---
+++ ConsoleCalc/NumberExpression.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCalc
+{
+    /// <summary>
+    /// Number (terminal expression)
+    /// </summary>
+    class NumberExpression : IExpression
+    {
+        /// <summary>
+        /// Variable name
+        /// </summary>
+        string name;
+
+        public NumberExpression(string variableName)
+        {
+            name = variableName;
+        }
+
+        public double Interpret(Context context)
+        {
+            return context.GetVariable(name);
+        }
+    }
+}
