c2e9e67530a01dbcd57bcc32c98315ae295d3e17
eugene <eugenerf@mail.ru>
2020-05-06 16:36:31 +0300

    Interpret pattern testing

--- ConsoleCalc/Program.cs
+++ ConsoleCalc/Program.cs
@@ -9,6 +9,18 @@ namespace ConsoleCalc
         {
             Console.WriteLine("---== eugene Console Calculator ==---");
 
+            Context context = new Context();
+
+            context.SetVariable("x", 5);
+            context.SetVariable("y", 2);
+            context.SetVariable("z", 8);
+
+            IExpression expression = new Add(
+                new Number("x"), new Subtract(
+                    new Number("y"), new Number("z")));
+
+            Console.WriteLine($"Result: {expression.Interpret(context)}");
+
             Console.Write("Any key to exit...");
             Console.ReadKey(true);
         }
