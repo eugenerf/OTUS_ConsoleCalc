214296dfc042ee9bbfd9b7d0a9a830817ea1f04c
eugene <eugenerf@mail.ru>
2020-05-06 16:30:10 +0300

    Added Subtration operation

---
+++ ConsoleCalc/Subtract.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleCalc
+{
+    /// <summary>
+    /// Subtract operation
+    /// </summary>
+    class Subtract : IExpression
+    {
+        /// <summary>
+        /// Left operand
+        /// </summary>
+        IExpression leftOperand;
+        /// <summary>
+        /// Right operand
+        /// </summary>
+        IExpression rightOperand;
+
+        public Subtract(IExpression left, IExpression right)
+        {
+            leftOperand = left;
+            rightOperand = right;
+        }
+
+        public double Interpret(Context context)
+        {
+            return leftOperand.Interpret(context) - rightOperand.Interpret(context);
+        }
+    }
+}
